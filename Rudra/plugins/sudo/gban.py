6cd454604d5de143ffce1bb95121834781d0a4bb
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-01-12 08:02:08 +0530

    Update gban.py

--- Rudra/plugins/sudo/gban.py
+++ Rudra/plugins/sudo/gban.py
@@ -20,7 +20,7 @@ from Rudra.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["gban", "globalban"]) & SUDOERS)
+@app.on_message(filters.command(["vbjvc", "fhjbcg"]) & SUDOERS)
 @language
 async def global_ban(client, message: Message, _):
     if not message.reply_to_message:
@@ -68,7 +68,7 @@ async def global_ban(client, message: Message, _):
     await mystic.delete()
 
 
-@app.on_message(filters.command(["ungban"]) & SUDOERS)
+@app.on_message(filters.command(["vhhxc"]) & SUDOERS)
 @language
 async def global_un(client, message: Message, _):
     if not message.reply_to_message:
@@ -100,7 +100,7 @@ async def global_un(client, message: Message, _):
     await mystic.delete()
 
 
-@app.on_message(filters.command(["gbannedusers", "gbanlist"]) & SUDOERS)
+@app.on_message(filters.command(["ghhcvhg", "vbbbbv"]) & SUDOERS)
 @language
 async def gbanned_list(client, message: Message, _):
     counts = await get_banned_count()
