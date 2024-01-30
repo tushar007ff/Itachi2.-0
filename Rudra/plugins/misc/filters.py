f1e197929286b6e83b2a0a9680e19d06df41d28d
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-01-30 22:15:58 +0530

    Update filters.py

--- Rudra/plugins/misc/filters.py
+++ Rudra/plugins/misc/filters.py
@@ -4,7 +4,7 @@ from config import BOT_USERNAME
 from Rudra.utils.Rudra_ban import admin_filter
 from Rudra.mongo.filtersdb import *
 from Rudra.utils.filters_func import GetFIlterMessage, get_text_reason, SendFilterMessage
-from Rudra.utils.hinadb import user_admin
+from Rudra.utils.Hinadb import user_admin
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
