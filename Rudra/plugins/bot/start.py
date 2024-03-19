886b951e02f418adeed553162abdf18053f43af6
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-03-19 20:05:26 +0530

    Update start.py

--- Rudra/plugins/bot/start.py
+++ Rudra/plugins/bot/start.py
@@ -43,7 +43,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_photo(
+            return await message.reply_video(
                 random.choice(HINA_VID_URL),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
@@ -98,7 +98,7 @@ async def start_pm(client, message: Message, _):
         served_chats = len(await get_served_chats())
         served_users = len(await get_served_users())
         UP, CPU, RAM, DISK = await bot_sys_stats()
-        await message.reply_photo(
+        await message.reply_video(
             random.choice(HINA_VID_URL),
             caption=random.choice(Rudraop).format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
             reply_markup=InlineKeyboardMarkup(out),
@@ -115,7 +115,7 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_photo(
+    await message.reply_video(
         random.choice(HINA_VID_URL),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
@@ -150,7 +150,7 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                await message.reply_photo(
+                await message.reply_video(
                     random.choice(HINA_VID_URL),
                     caption=_["start_3"].format(
                         message.from_user.mention,
