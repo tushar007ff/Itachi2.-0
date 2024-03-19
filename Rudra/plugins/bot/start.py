baf5b38b9e131ee63529a6bf5d9659bf63cff79c
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-03-19 19:53:11 +0530

    Update start.py

--- Rudra/plugins/bot/start.py
+++ Rudra/plugins/bot/start.py
@@ -44,7 +44,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
-                random.choice(Hina_PICS),
+                random.choice(HINA_IMG_URL),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -99,7 +99,7 @@ async def start_pm(client, message: Message, _):
         served_users = len(await get_served_users())
         UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_photo(
-            random.choice(Hina_PICS),
+            random.choice(HINA_IMG_URL),
             caption=random.choice(Rudraop).format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -116,7 +116,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        random.choice(Hina_PICS),
+        random.choice(HINA_IMG_URL),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -151,7 +151,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    random.choice(Hina_PICS),
+                    random.choice(HINA_IMG_URL),
                     caption=_["start_3"].format(
                         message.from_user.mention,
                         app.mention,
