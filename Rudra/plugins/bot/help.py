759510ed4bacb385dfa048812e7dd1d9c9b2dc5d
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-03-19 16:16:39 +0530

    Update help.py

--- Rudra/plugins/bot/help.py
+++ Rudra/plugins/bot/help.py
@@ -45,7 +45,7 @@ async def helper_private(
         )
 
 
-@app.on_message(filters.command(["hgghk"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
